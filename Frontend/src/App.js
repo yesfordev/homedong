56933711a9094eb6120af2d68bdcb66ddbc69ee1
sangmin song <ssmin940606@gmail.com>
2021-07-23 00:45:11 +0900

    CHORE: react-router 추가

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-// import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route } from 'react-router-dom';
+import styled from 'styled-components';
+import SignUp from './features/signup/SignUp';
+import Home from './features/home/Home';
+
+const Wrapper = styled.div`
+  background-color: rgba(246, 245, 253, 1);
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div>
-      <h1>Hello, world!</h1>
-    </div>
+    <Wrapper>
+      <BrowserRouter>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup" component={SignUp} />
+      </BrowserRouter>
+    </Wrapper>
   );
 }
 
