8680f168b51c5cc32327730bc53e3c63651fefab
sangmin song <ssmin940606@gmail.com>
2021-08-04 19:00:33 +0900

    FEAT: 캘린더내 이미지 삽입 구현 [#S05P13A608-71] - 특정 날짜에 이미지 삽입 기능 구현

--- frontend/src/features/mypage/Calender.jsx
+++ frontend/src/features/mypage/Calender.jsx
@@ -1,9 +1,22 @@
 import React, { useState } from 'react';
 import { Calendar as Main } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import Badge1 from '../../assets/badge1.png';
 
 const content = ({ date, view }) => {
-  return view === 'month' && date.getDay() === 2 ? '😀' : null;
+  // test dates
+  const dates = [
+    [7, 1],
+    [7, 3],
+    [7, 5],
+  ];
+  return dates.map(([m, d]) => {
+    return view === 'month' &&
+      date.getMonth() === m - 1 &&
+      date.getDate() === d ? (
+      <img alt="prize" src={Badge1} style={{ width: '70%' }} />
+    ) : null;
+  });
 };
 
 const onClick = (V) => {
