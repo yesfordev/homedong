71f8c031d2ab69273477861cfedd2ae60f77f761
yesfordev <yesfordev@gmail.com>
2021-08-10 16:23:16 +0900

    FEAT: 이메일 인증 폼 추가 [#S05P13A608-187]

--- backend/src/main/java/com/calisthenics/homedong/api/controller/MailController.java
+++ backend/src/main/java/com/calisthenics/homedong/api/controller/MailController.java
@@ -1,5 +1,6 @@
 package com.calisthenics.homedong.api.controller;
 
+import com.calisthenics.homedong.api.request.MailReq;
 import com.calisthenics.homedong.api.service.MailService;
 import com.calisthenics.homedong.error.ErrorResponse;
 import io.swagger.annotations.*;
@@ -22,19 +23,15 @@ import java.net.UnknownHostException;
 public class MailController {
     private final MailService mailService;
 
-    @GetMapping("/api/mail")
+    @PostMapping("/api/mail")
     @ApiOperation(value = "이메일 전송 테스트", notes = "<strong>이메일</strong>를 통해 이메일 전송 테스트를 한다.")
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "email", value = "메일을 받을 email 주소", required = true, dataType = "String", paramType = "query"),
-            @ApiImplicitParam(name = "nickname", value = "메일을 받을 닉네임", required = true, dataType = "String", paramType = "query"),
-    })
     @ApiResponses({
             @ApiResponse(code = 200, message = "전송 성공"),
             @ApiResponse(code = 400, message = "input 오류", response = ErrorResponse.class),
             @ApiResponse(code = 500, message = "서버 오류", response = ErrorResponse.class)
     })
-    public ResponseEntity sendMail(@RequestParam String email, @RequestParam String nickname) throws UnknownHostException, MessagingException {
-        mailService.sendAuthMail(email, nickname);
+    public ResponseEntity sendMail(@RequestBody MailReq mailReq) throws UnknownHostException, MessagingException {
+        mailService.sendAuthMail(mailReq.getEmail(), mailReq.getNickname());
 
         return new ResponseEntity(HttpStatus.OK);
     }
--- backend/src/main/java/com/calisthenics/homedong/api/service/MailService.java
+++ backend/src/main/java/com/calisthenics/homedong/api/service/MailService.java
@@ -57,26 +57,31 @@ public class MailService {
         emailcontent.append("<body>");
         emailcontent.append(
                 " <div" 																																																	+
-                        "	style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 400px; height: 600px; border-top: 4px solid #FFD43A; margin: 100px auto; padding: 30px 0; box-sizing: border-box;\">"		+
-                        "	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400;\">"																															+
-                        "		<span style=\"font-size: 15px; margin: 0 0 10px 3px;\">HOMEDONG</span><br />"																													+
-                        "		<span style=\"color: #6A60A9\">메일인증</span> 안내입니다."																																				+
-                        "	</h1>\n"																																																+
-                        "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px;\">"																													+
-                        nickname																																																+
-                        "		님 안녕하세요.<br />"																																													+
-                        "		HOMEDONG에 가입해 주셔서 진심으로 감사드립니다.<br />"																																						+
-                        "		아래 <b style=\"color: #6A60A9\">'메일 인증'</b> 버튼을 클릭하여 회원가입을 완료해 주세요.<br />"																													+
-                        "		감사합니다."																																															+
-                        "	</p>"																																																	+
-                        "	<a style=\"color: #FFF; text-decoration: none; text-align: center;\""																																	+
-                        "	href=\"http://" + serverAddress + ":" + serverPort + "/api/signup/confirm?email=" + email + "&authKey=" + authKey + "\" target=\"_blank\">"														+
-                        "		<p"																																																	+
-                        "			style=\"display: inline-block; width: 210px; height: 45px; margin: 30px 5px 40px; background: #6A60A9; line-height: 45px; vertical-align: middle; font-size: 16px;\">"							+
-                        "			메일 인증</p>"																																														+
-                        "	</a>"																																																	+
-                        "	<div style=\"border-top: 1px solid #FFD43A; padding: 5px;\"></div>"																																		+
-                        " </div>"
+                "	style=\"font-family: 'Apple SD Gothic Neo', 'sans-serif' !important; width: 500px; height: 600px; border-top: 4px solid #FFD43A; margin: 100px auto; padding: 30px 0; box-sizing: border-box; color: #000000;\">"		+
+//                "<style> img { display: block; margin: 0px auto; } </style>" +
+                "<img src=\"https://user-images.githubusercontent.com/31542907/128824079-c8368805-bafa-4366-8f29-84bd3bc17251.png\" " +
+                "style=\"margin: 0 auto; display: block;\" width=\"350\"/>" +
+                "	<h1 style=\"margin: 0; padding: 0 5px; font-size: 28px; font-weight: 400; text-align: center;\">"																															+
+//                "		<span style=\"font-size: 15px; margin: 0 0 10px 3px;\">HOMEDONG</span><br />"																												+
+                "		<span style=\"color: #6A60A9; text-align: center;\">메일인증</span> 안내입니다."																																				+
+                "	</h1>\n"																																																+
+                "	<p style=\"font-size: 16px; line-height: 26px; margin-top: 50px; padding: 0 5px; color: #000000;\">"																													+
+                nickname + " ( " + email + " ) "																																																+
+                "		님 안녕하세요.<br />"																																													+
+                "		HOMEDONG에 가입해 주셔서 진심으로 감사드립니다.<br />"																																						+
+                "		아래 <b style=\"color: #6A60A9\">'메일 인증'</b> 버튼을 클릭하여 회원가입을 완료해 주세요.<br />"																													+
+                "		감사합니다."																																															+
+                "	</p>"																																																	+
+                "	<a style=\"color: #FFF; text-decoration: none; text-align: center;\""																																	+
+                "	href=\"http://" + serverAddress + ":" + serverPort + "/api/signup/confirm?email=" + email + "&authKey=" + authKey + "\" target=\"_blank\">"														+
+                "<div style=\"margin: 0 auto;\">" +
+                "		<p"																																																	+
+                "			style=\"display: inline-block; width: 210px; height: 45px; margin: 30px 5px 50px; background: #6A60A9; line-height: 45px; vertical-align: middle; font-size: 16px;\">"							+
+                "			메일 인증</p>" +
+                "</div>"+
+                "	</a>"																																																	+
+                "	<div style=\"border-top: 1px solid #FFD43A; padding: 5px;\"></div>"																																		+
+                " </div>"
         );
         emailcontent.append("</body>");
         emailcontent.append("</html>");
