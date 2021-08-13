76bbbaf56cfee2355606ee05318a8956798ea284
qlcid <qlcid@naver.com>
2021-08-13 22:37:26 +0900

    FIX: mixed content 에러 수정

--- frontend/src/common/api/http-common.js
+++ frontend/src/common/api/http-common.js
@@ -2,7 +2,7 @@ import baseAxios from 'axios';
 import { getToken } from './JWT-common';
 
 const axios = baseAxios.create({
-  baseURL: 'http://i5a608.p.ssafy.io',
+  baseURL: 'https://i5a608.p.ssafy.io',
   headers: {
     'Content-Type': 'application/json',
   },
