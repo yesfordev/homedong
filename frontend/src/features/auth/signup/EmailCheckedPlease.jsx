ff46d7024e67e825da782ce73cf5953e883dfcc6
yesfordev <yesfordev@gmail.com>
2021-08-17 01:47:25 +0900

    FEAT: [back] 이메일 인증 리다이렉트 로직 추가 [#S05P13A608-251, S05P13A608-248]

--- frontend/src/features/auth/signup/EmailCheckedPlease.jsx
+++ frontend/src/features/auth/signup/EmailCheckedPlease.jsx
@@ -58,7 +58,7 @@ export default function EmailCheckedPlease() {
       <br />
       <div>
         <CommonButton mauve="true">
-          <Link to="/login">로그인 하러 가기</Link>
+          <Link to="/login">홈으로</Link>
         </CommonButton>
       </div>
     </Wrapper>
