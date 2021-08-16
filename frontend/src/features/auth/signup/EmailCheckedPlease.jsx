3e28c3c5b9e37550a2fe0e6beed03272f6bb4776
yesfordev <yesfordev@gmail.com>
2021-08-17 01:39:47 +0900

    FEAT: 이메일 인증 페이지 추가 [#S05P13A608-248, #S05P13A608-251 ]

--- frontend/src/features/auth/signup/EmailCheckedPlease.jsx
+++ frontend/src/features/auth/signup/EmailCheckedPlease.jsx
@@ -47,13 +47,13 @@ export const CommonButton = styled(Button)`
   }
 `;
 
-export default function EmailChecked() {
+export default function EmailCheckedPlease() {
   return (
     <Wrapper>
       <Logo src={logo} width="60vh" alt="logo" />
       <Title>
-        이메일 인증이 완료되었습니다😀 <br /> <br />
-        로그인 후 서비스 이용 부탁드립니다
+        HOMEDONG 회원가입을 축하드립니다🎉 <br /> <br />
+        모든 서비스를 이용하기 위해 이메일 인증을 완료하여 주세요
       </Title>
       <br />
       <div>
