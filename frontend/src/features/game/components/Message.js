12e26f4855f199567d9657b5f730c435255737db
sangmin song <ssmin940606@gmail.com>
2021-08-18 12:18:06 +0900

    STYLE: [front] 게임 패이지 채팅창 스타일링 [#S05P13A608-332]

--- frontend/src/features/game/components/Message.js
+++ frontend/src/features/game/components/Message.js
@@ -1,17 +1,32 @@
 /* eslint-disable react/self-closing-comp */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const Username = styled.p`
+  color: #42387a;
+  font-size: 0.8rem;
+  font-weight: 600;
+  padding: 5px 0 10px 0;
+`;
+
+const MessageContainer = styled.div`
+  width: 100px;
+`;
+
+const Text = styled.p`
+  font-size: 1rem;
+`;
 
 class Message extends Component {
   render() {
     const { text, userName } = this.props;
 
     return (
-      <div>
-        <b>{userName}</b>
-        <br></br>
-        {text}
-      </div>
+      <MessageContainer>
+        <Username>{userName}</Username>
+        <Text>{text}</Text>
+      </MessageContainer>
     );
   }
 }
--- frontend/src/features/game/components/Messages.js
+++ frontend/src/features/game/components/Messages.js
@@ -1,15 +1,18 @@
 /* eslint-disable react/no-array-index-key */
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Message from './Message';
+
+const ChatContainer = styled.div``;
 
 class Messages extends Component {
   render() {
     const { messages } = this.props;
 
     return messages.map((message, i) => (
-      <div className={`messages__item ${message.chatClass}`} key={i}>
+      <ChatContainer className={`messages__item ${message.chatClass}`} key={i}>
         <Message text={message.text} userName={message.userName} />
-      </div>
+      </ChatContainer>
     ));
   }
 }
